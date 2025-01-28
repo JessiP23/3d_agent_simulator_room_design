58c66f723525b8694fba397dbc8c6efc93b7cb48
JessiP23 <jessi316866@gmail.com>
2025-01-27 22:25:03 -0500

    room to improve features

---
+++ room-to-improve.py
@@ -0,0 +1,18 @@
+# num of users: 5000
+# num of songs: 10000
+# mp3 audio: 5MB
+# Total audio: 5MB * 10000 = 50GB
+# Replication Factor: 3 * 50GB = 150GB
+# Metadata = 10B per song = 10000 * 10B = 100KB
+# 1KB per user = 5000KB = 5MB
+
+# Frontend (APP) UI
+# Load Balancer
+# Web Server
+# Database
+# CDN
+
+
+
+
+
